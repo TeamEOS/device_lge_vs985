a8ea528efb753609d3b779f618fa8639cc3e82e7
Joe Wooster <superhanss@aospal.com>
2015-06-08 13:31:51 +0000

    vs985: EOS bring-up
    Change-Id: I4cf8528fefd6d020acaf703e5ec6eb8a1ed4b3e0

--- eos.mk
+++ eos.mk
@@ -1,12 +1,16 @@
 $(call inherit-product, device/lge/vs985/full_vs985.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common EOS stuff.
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/eos/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_vs985
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+vendor/eos/prebuilt/common/bootanimation/1080.zip:system/media/bootanimation.zip
+
+PRODUCT_NAME := eos_vs985
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
